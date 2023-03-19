9129e404f9075df8675516f0ed320fe4b0d11d6a
chuwa-tech <chuwakapj@gmail.com>
2023-03-19 12:31:22 +0100

    cabecera para gestión de listas

---
+++ list_management.h
@@ -0,0 +1,9 @@
+// Fichero donde se definen los prototipos para el tratamiento de listas de bloques o inodos
+
+// Busqueda
+void search();
+// Borrado
+void delete();
+// Inserción nuevo elemento
+void insert();
+
