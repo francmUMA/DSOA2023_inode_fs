3b25766777928a84c7a5464850d89d76c374e1d7
francmUMA <francano368@gmail.com>
2023-04-06 17:45:16 +0200

    nuevas funciones

--- .ccls-cache/@home@fran@DSOA2023_inode_fs/tree_management.c
+++ .ccls-cache/@home@fran@DSOA2023_inode_fs/tree_management.c
@@ -1,9 +1,22 @@
 #include "data_structures_fs.h"
+#include "tree_search.c"
 #include <stdio.h>
 #include <stdlib.h>
 
 // Tengo que modificar nuestra estructura de árbol de búsqueda
-void insert(struct inode *root, struct inode n_node)
+void insert(struct inode *directory_dst, struct inode n_node)
 {
-    // Comenzamos en nuestro directorio raíz
+    int i = 0;
+    directory_entry *entry = malloc(sizeof(directory_entry));
+    while(i < N_DIRECTOS && directory_dst->i_directos[i] != NULL){
+        // Vamos a buscar el primer hueco libre en el bloque
+        // Recorremos el bloque
+        memcpy(entry, (*directory_dst).i_directos[i], sizeof(directory_entry));
+        for(int j = 0; j < 32 && (entry -> inode) != NULL; j++){ // j es offset
+            //Traemos la entrada de directorio
+            memcpy(entry, (*directory_dst).i_directos[i]+sizeof(directory_entry)*j, sizeof(directory_entry));
+            // Si el inodo es NULL, es que es un hueco libre
+        }
+        i++;
+    }
 }
